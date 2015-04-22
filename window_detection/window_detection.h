ca85609610f67fd99535517890b5e8f48edccdec
Joon-Hyuk <hanjh1@gmail.com>
2015-04-21 23:33:44 -0400

    window detection sort of working

---
+++ window_detection/window_detection.h
@@ -0,0 +1,9 @@
+/*
+using namespace cv;
+using namespace std;
+
+double angle( cv::Point pt1, cv::Point pt2, cv::Point pt0 ) 
+
+void find_squares(Mat& image, vector<vector<Point> >& squares)
+void find_largest_square(const vector<vector<Point> >& squares, vector<Point>& biggest_square)
+*/
